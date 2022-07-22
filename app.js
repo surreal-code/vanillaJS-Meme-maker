74f9bae424b816c125f2dd32f724541432f82b4f
surreal-code <yimina@naver.com>
2022-07-22 10:41:48 +0900

    update draw mode

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
@@ -10,6 +11,7 @@ canvas.height = 800;
 
 ctx.lineWidth = lineWidth.value;
 let isPaintng = false;
+let isFilling = false;
 
 function onMove (event) {
     if(isPaintng) {
@@ -46,16 +48,34 @@ function onColorClick(event) {
     color.value = colorValue;
 }
 
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 
+modeBtn.addEventListener("click", onModeClick);
 
 /* draw color line
 const colors = [
